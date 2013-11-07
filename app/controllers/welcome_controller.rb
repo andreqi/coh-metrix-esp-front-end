6f4d1745f52353916b37a5f514db090437199b24
Andre Quispesaravia <drol.005@gmail.com>
2013-11-06 21:02:27 -0500

    initial commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,32 @@
+require 'net/http'
+require 'json'
+class HttpRequestClient
+  def self.doRequest(url, params)
+    url = URI.parse(url)
+    req = Net::HTTP::Post.new(url.path)
+    req.set_form_data params
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+    res.body
+  end
+end
+
+class CohMetrixEspClient
+  def self.callCohMetrix(text)
+    ans = JSON::parse(HttpRequestClient::doRequest 'http://localhost:4567/', :text => text)
+    [ans["metrics"], ans["class"]]
+  end
+
+end 
+
+
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def analyze
+    @metrics, @class = CohMetrixEspClient::callCohMetrix params[:texto]
+    @text = params[:texto]
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CohMetrix::Application.config.secret_token = '66f2e74ae1df643f520ffafc13af5acd8308af945198f031ad056ee5bd84782be43695374a4eb34c40488b8fa5b715a10967e666a00480e14430952d89fcd3e6'
